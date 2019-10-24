5995d30827003ad6ae1835cdc84694d229005c30
Pete Hanner <petehanner@gmail.com>
2019-10-24 12:29:39 -0400

    Done.

--- js/reducer.js
+++ js/reducer.js
@@ -1,1 +1,35 @@
-// add code snippets from README
+// declare state
+let state;
+
+// give reducer default state value and switch for action types
+function reducer(state = { count: 0 }, action) {
+  switch (action.type) {
+    case 'INCREASE_COUNT':
+      return { count: state.count + 1 };
+    default:
+      return state;
+  }
+}
+
+// dispatch invokes reducer w/ specified action and re-renders
+function dispatch(action) {
+  state = reducer(state, action);
+  render();
+}
+
+// HTML doc has #container element and button, otherwise totally boilerplate
+
+// render state in #container text
+function render() {
+  const container = document.getElementById('container');
+  container.textContent = state.count;
+}
+
+// invoke dispatch with 'INCREASE_COUNT' action type on click
+const button = document.getElementById('button');
+button.addEventListener('click', (e) => {
+  dispatch({ type: 'INCREASE_COUNT' })
+});
+
+// Display default state on page load
+dispatch({ type: '@@INIT' })
